247b10ce4e01fe92ab7e4803ff6a5b33f0ccc473
flyingisawol <akeir@me.com>
2020-10-02 16:00:16 +1000

    fix undefined variables

--- main.js
+++ main.js
@@ -3,18 +3,18 @@
         function computerPlay() {
         const arr = ['paper', 'scissors', 'rock'];
         const randomIndex = Math.floor(Math.random() * arr.length);
-        return (arr[randomIndex]);
+        return playerSelection = (arr[randomIndex]);
         }
 
 // function to play single round of paper, scissors, rock.
         function singleRound() {
+        const arr = ['paper', 'scissors', 'rock'];
         const playerSelection = prompt("Enter Selection: Paper, Scissors or Rock");
-        const arr = ['paper', 'scissors', 'rock']; 
-        if (playerSelection == rock && computerPlay == scissors) {
-          result = "You Win. Rock Smashes Scissors";
-          } else if (playerSelection == rock && computerPlay == paper) {
-            result = "You Lose. Paper beats Rock";
-          } else if (playerSelection == rock && computerPlay == rock) {
+        if (playerSelection == 'rock' && computerPlay() == 'scissors') {
+                return "You Win. Rock Smashes Scissors";    
+          } else if (playerSelection == 'rock' && computerPlay() == 'paper') {
+                  result = "You Lose. Paper beats Rock";
+          } else if (playerSelection == 'rock' && computerPlay() == 'rock') {
                   result = "Draw"; 
           }        
-        }
+        }       
