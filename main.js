57e2fbc91b1845ba3e797b33d52a3d07751d3d45
flyingisawol <akeir@me.com>
2020-10-02 16:20:12 +1000

    fix undefined variables

--- main.js
+++ main.js
@@ -8,15 +8,16 @@
 
 // function to play single round of paper, scissors, rock.
         function singleRound() {
+        const computerSelection = computerPlay();
         const arr = ['paper', 'scissors', 'rock'];
         const playerSelection = prompt("Enter Selection: Paper, Scissors or Rock");
-        if (playerSelection == 'rock' && computerPlay() == 'scissors') {
+        if (playerSelection == 'rock' && computerSelection == 'scissors') {
                 return "You Win. Rock Smashes Scissors";    
-          } else if (playerSelection == 'rock' && computerPlay() == 'paper') {
+          } else if (playerSelection == 'rock' && computerSelection == 'paper') {
                   return "You Lose. Paper beats Rock";
-          } else if (playerSelection == 'rock' && computerPlay() == 'rock') {
+          } else if (playerSelection == 'rock' && computerSelection == 'rock') {
                   return "Draw"; 
-          } else if (playerSelection == 'paper' && computerPlay() == 'rock') {
+          } else if (playerSelection == 'paper' && computerSelection == 'rock') {
                   return "You Win. Paper beats Rock";
           }
         }       
